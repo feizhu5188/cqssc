3502515f33bee623170adc5043a23c83d1154424
feizhu5188 <958846487@qq.com>
2018-02-02 21:51:38 +0800

    测试提交，添了一个测试类

---
+++ selenium_fgw/ceshi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_fgw
+{
+    class ceshi
+    {
+
+        public void ceshi()
+        {
+            System.Windows.Forms.MessageBox.Show("Test");
+
+        }
+        
+    }
+}
